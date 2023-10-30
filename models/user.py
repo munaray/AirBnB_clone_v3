a95460586f10454125acec5765285386f968a1e1
Ronnie-Leon76 <ronleon76@gmail.com>
2023-10-30 14:30:41 +0300

    feat: All tasks completed

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -26,4 +27,10 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if kwargs:
+            if 'password' in kwargs:
+                password = kwargs['password']
+                password = hashlib.md5(password.encode())
+                password = password.hexdigest()
+                kwargs['password'] = password
         super().__init__(*args, **kwargs)
